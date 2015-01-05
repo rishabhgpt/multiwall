e46fba5951f58a22caa074547eb7d2790ad26ffc
Rishabh Gupta <rishabhgpt3@gmail.com>
2015-01-05 09:12:18 +0530

    Update n.sh

--- n.sh
+++ n.sh
@@ -1,3 +1,3 @@
-id=$(wget -q http://bing.com -O - | grep -o http://bing.com -O - | grep -o url:[^' ']*jpg|cut -c6-|rev|cut -c13-|rev)
+id=$(wget -q http://bing.com -O - | grep -o url:[^' ']*jpg|cut -c6-|rev|cut -c13-|rev)
 wget http://bing.com"$id"
 mv *.jpg image.jpg
