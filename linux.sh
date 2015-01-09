287add73c4021c818410c91d2183a247237f7c68
Rishabh Gupta <2011uit1465@mnit.ac.in>
2015-01-10 02:09:59 +0530

    Updated linux.sh

--- linux.sh
+++ linux.sh
@@ -1,5 +1,13 @@
-sh n.sh
-PIC=$(pwd)
-PIC=$PIC'/image.jpg'
-echo $PIC
-gsettings set org.gnome.desktop.background picture-uri 'file://$PIC'
+#!/bin/bash
+export http_proxy=""
+export https_proxy=""
+wget -q imgur.com/r/wallpapers
+grep -oE id=[^\\]{10}class=\"post\"\> wallpapers>walls
+IFS=$'\n'      
+for j in `cat ./walls`
+do
+j=$(echo $j|cut -c5-|rev|cut -c16-|rev)
+wget -nc http://imgur.com/$j.jpg -P /tmp
+gsettings set org.gnome.desktop.background picture-uri "file:///tmp/$j.jpg"
+sleep 1800
+done
